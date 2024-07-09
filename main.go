bf3cf1e79ad54d93113d9b5c6243dcf5036fd9f1
Azamat Zulpykhar <spender.poised-0q@icloud.com>
2024-07-09 10:48:48 +0500

    Added hello handler

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Data struct {
+	Name string `json:"name"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+
+		json.NewEncoder(w).Encode(Data{
+			Name: "John Doe",
+		})
+	})
+
+	http.ListenAndServe("localhost:3000", nil)
 }
